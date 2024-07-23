56b7dddbf77ebf9954f5f879ac159179bcf11703
ifanani-code <ifanani63@gmail.com>
2024-07-23 20:28:20 +0700

    dark mode

--- js/script.js
+++ js/script.js
@@ -1,38 +1,77 @@
 // navbar fixed
-window.onscroll = function() {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
 };
 
 // hamburger
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', function(){
-    hamburger.classList.toggle('hamburger-active');
-    navMenu.classList.toggle('hidden');
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
 });
 
 // contact form
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwgUPf6gkKIhGtLTAGRNgAtsRSrFnenFYbjAqCeVvwBA3V4SX5p9wW6y6Cbb6I6WJai/exec'
-const form = document.forms['submit-to-google-sheet']
-const msg = document.getElementById("msg")
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbwgUPf6gkKIhGtLTAGRNgAtsRSrFnenFYbjAqCeVvwBA3V4SX5p9wW6y6Cbb6I6WJai/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
 
-form.addEventListener('submit', e => {
-e.preventDefault()
-fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        msg.innerHTML = "Message sent successfully!"
-        setTimeout(function(){
-            msg.innerHTML = ""
-        },2500)
-        form.reset()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully!";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 2500);
+      form.reset();
     })
-    .catch(error => console.error('Error!', error.message))
-})
+    .catch((error) => console.error("Error!", error.message));
+});
+
+// klik di luar hamburger
+window.addEventListener("click", function (e) {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
+
+// dark mode toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+// moving toggle position
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
