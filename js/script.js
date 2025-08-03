b25faaddb2369281b1c6b52772280a13505e857c
ifanani-code <ifanani63@gmail.com>
2025-08-03 22:50:11 +0700

    hide contact section, adding new expertise & portfolio

--- js/script.js
+++ js/script.js
@@ -25,23 +25,23 @@ hamburger.addEventListener("click", function () {
 });
 
 // contact form
-const scriptURL =
-  "https://script.google.com/macros/s/AKfycbwgUPf6gkKIhGtLTAGRNgAtsRSrFnenFYbjAqCeVvwBA3V4SX5p9wW6y6Cbb6I6WJai/exec";
-const form = document.forms["submit-to-google-sheet"];
-const msg = document.getElementById("msg");
+// const scriptURL =
+//   "https://script.google.com/macros/s/AKfycbwgUPf6gkKIhGtLTAGRNgAtsRSrFnenFYbjAqCeVvwBA3V4SX5p9wW6y6Cbb6I6WJai/exec";
+// const form = document.forms["submit-to-google-sheet"];
+// const msg = document.getElementById("msg");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => {
-      msg.innerHTML = "Message sent successfully!";
-      setTimeout(function () {
-        msg.innerHTML = "";
-      }, 2500);
-      form.reset();
-    })
-    .catch((error) => console.error("Error!", error.message));
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) => {
+//       msg.innerHTML = "Message sent successfully!";
+//       setTimeout(function () {
+//         msg.innerHTML = "";
+//       }, 2500);
+//       form.reset();
+//     })
+//     .catch((error) => console.error("Error!", error.message));
+// });
 
 // klik di luar hamburger
 window.addEventListener("click", function (e) {
@@ -74,4 +74,24 @@ if (
   darkToggle.checked = true;
 } else {
   darkToggle.checked = false;
+}
+
+// animasi
+window.addEventListener("scroll", reveal);
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add("animate__animated");
+      reveals[i].classList.add("animate__fadeInDown");
+    } else {
+      reveals[i].classList.remove("animate__animated");
+      reveals[i].classList.remove("animate__fadeInDown");
+    }
+  }
 }
